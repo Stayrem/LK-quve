1b303ed41957fccaa0929a3ff379de848cd4dbfc
Dmitriy <spirin2205@yandex.ru>
2020-09-17 01:25:48 +0300

    added Overview page preloader

---
+++ src/preloaders/ExpensesListPreloader/ExpensesListPreloader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './ExpensesListPreloader.scss';
+
+const ExpensesListPreloader = () => {
+  const { expences } = styles;
+  return <div className={expences} />;
+};
+
+export default ExpensesListPreloader;
---
+++ src/preloaders/SaldoPreloader/SaldoPreloader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './SaldoPreloader.scss';
+
+const SaldoPreloader = () => {
+  const { saldo } = styles;
+  return <div className={saldo} />;
+};
+
+export default SaldoPreloader;
