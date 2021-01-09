f1ed9283686fb506b3750271af7ae319ccf926e7
User <user@MacBook-User.local>
2021-01-10 00:30:07 +0300

    feat: added new redux structure

--- src/store/action-creator.js
+++ src/store/action-creator.js
@@ -1,174 +1,104 @@
-import dictionary from '../utils/dictionary';
+import moment from 'moment';
+import 'moment/locale/ru';
 import Type from './action-types';
 import fetchData from '../utils/fetch';
 
-export const fetchDateData = (data) => ({
-  type: Type.FETCH_DATE_DATA,
+export const setUserInfo = (data) => ({
+  type: Type.SET_USER_INFO,
   payload: data,
 });
 
-export const fetchIncomesData = (data) => ({
-  type: Type.FETCH_INCOMES_DATA,
-  payload: data,
-});
-
-export const fetchCostsData = (data) => ({
-  type: Type.FETCH_COSTS_DATA,
-  payload: data,
-});
-
-export const fetchSavingsData = (data) => ({
-  type: Type.FETCH_SAVINGS_DATA,
-  payload: data,
-});
-
-export const fetchSpendingsData = (data) => ({
-  type: Type.FETCH_SPENDINGS_DATA,
-  payload: data,
-});
-
-export const fetchSpendingsHistoryData = (data) => ({
-  type: Type.FETCH_SPENDINGS_HISTORY_DATA,
-  payload: data,
-});
-
-export const fetchSaldoHistoryData = (data) => ({
-  type: Type.FETCH_SALDO_HISTORY_DATA,
-  payload: data,
-});
-
-export const fetchSavingsHistoryData = (data) => ({
-  type: Type.FETCH_SAVINGS_HISTORY_DATA,
-  payload: data,
-});
-
-export const setDate = (data) => ({
-  type: Type.SET_DATE,
-  payload: data,
-});
-
-export const setIncomesData = (data) => ({
+export const setIncomes = (data) => ({
   type: Type.SET_INCOMES_DATA,
   payload: data,
 });
 
-export const setCostsData = (data) => ({
+export const setCosts = (data) => ({
   type: Type.SET_COSTS_DATA,
   payload: data,
 });
 
-export const setSpendingsData = (data) => ({
-  type: Type.SET_SPENDINGS_DATA,
+export const setMounthSpendings = (data) => ({
+  type: Type.SET_MOUNTH_SPENDINGS_DATA,
   payload: data,
 });
 
-export const setSavingsData = (data) => ({
-  type: Type.SET_SAVINGS_DATA,
+export const setSavings = (data) => ({
+  type: Type.SET_SAVINGS,
   payload: data,
 });
 
-export const getDateData = () => async (dispatch) => {
+export const setOverviewData = (obj) => ({
+  type: Type.SET_OVERVIEW_DATA,
+  payload: obj,
+});
+
+export const setDate = (epoch) => ({
+  type: Type.SET_DATE,
+  payload: epoch,
+});
+
+export const fetchUserInfo = () => async (dispatch) => {
   try {
-    const data = await fetchData('/mocks/date/get.json');
-    dispatch(fetchDateData(data));
+    const userInfo = await fetchData('/mocks/info.json');
+    dispatch(setUserInfo(userInfo.data));
   } catch (err) {
     console.log(err);
   }
 };
 
-export const getIncomesData = () => async (dispatch) => {
-  try {
-    const data = await fetchData('/mocks/incomes/get.json');
-    dispatch(fetchIncomesData(data));
-  } catch (err) {
-    console.log(err);
-  }
+export const fetchIncomes = () => async (dispatch) => {
+  const incomes = await fetchData('/mocks/incomes.json');
+  dispatch(setIncomes(incomes.data));
 };
 
-export const getCostsData = () => async (dispatch) => {
-  try {
-    const data = await fetchData('/mocks/costs/get.json');
-    dispatch(fetchCostsData(data));
-  } catch (err) {
-    console.log(err);
-  }
+export const fetchCosts = () => async (dispatch) => {
+  const costs = await fetchData('/mocks/costs.json');
+  dispatch(setCosts(costs.data));
 };
 
-export const getSavingsData = () => async (dispatch) => {
-  try {
-    const data = await fetchData('/mocks/savings/get.json');
-    dispatch(fetchSavingsData(data));
-  } catch (err) {
-    console.log(err);
-  }
+export const fetchSpendings = () => async (dispatch) => {
+  const spendings = await fetchData('/mocks/spendings.json');
+  dispatch(setMounthSpendings(spendings.data));
 };
 
-export const getSpendingsData = () => async (dispatch) => {
-  try {
-    const data = await fetchData('/mocks/spendings/get.json');
-    dispatch(fetchSpendingsData(data));
-  } catch (err) {
-    console.log(err);
-  }
+export const fetchSavings = () => async (dispatch) => {
+  const savings = await fetchData('/mocks/savings.json');
+  dispatch(setSavings(savings.data));
 };
 
-export const getHistoryData = (type) => async (dispatch) => {
-  let url;
-  let dispatchFunction;
+export const getOverviewData = () => async (dispatch, getState) => {
+  await dispatch(fetchSpendings());
+  await dispatch(fetchIncomes());
+  await dispatch(fetchCosts());
+  await dispatch(fetchSavings());
+  const {
+    savings,
+    incomes,
+    mounthSpendings,
+    date,
+  } = getState();
+  const currentSavingSum = (savings[savings.length - 1]
+    .percent * incomes.reduce((acc, current) => acc + current.value, 0)) / 100;
+  const incomesSum = incomes.reduce((acc, current) => acc + current.value, 0);
+  const mounthSpendingsSum = mounthSpendings.reduce((acc, current) => acc + current.value, 0);
+  /*
+    const selectedDaySpendings = mounthSpendings.filter((item) => {
+    const selectedDay = moment(date * 1000).format('YYYY-MM-DD');
+    const storedDay = moment(item.date * 1000).format('YYYY-MM-DD');
+    return selectedDay === storedDay;
+  });
+  */
+  const selectedDaySpendings = mounthSpendings;
+  const daySpendingsSum = selectedDaySpendings.reduce((acc, current) => acc + current.value, 0);
+  const moneyRemains = incomesSum - mounthSpendingsSum - currentSavingSum;
 
-  switch (type) {
-    case dictionary.HISTORY_TYPE_SPENDINGS:
-      url = '/mocks/history/spendings.json';
-      dispatchFunction = (data) => fetchSpendingsHistoryData(data);
-      break;
-    case dictionary.HISTORY_TYPE_SALDO:
-      url = '/mocks/history/saldo.json';
-      dispatchFunction = (data) => fetchSaldoHistoryData(data);
-      break;
-    case dictionary.HISTORY_TYPE_SAVINGS:
-      url = '/mocks/history/savings.json';
-      dispatchFunction = (data) => fetchSavingsHistoryData(data);
-      break;
-    default:
-      break;
-  }
-
-  try {
-    const data = await fetchData(url);
-    dispatch(dispatchFunction(data));
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-export const updateSpendingsData = (data) => async (dispatch) => {
-  try {
-    dispatch(setSpendingsData(data));
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-export const updateIncomesData = (data) => async (dispatch) => {
-  try {
-    dispatch(setIncomesData(data));
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-export const updateCostsData = (data) => async (dispatch) => {
-  try {
-    dispatch(setCostsData(data));
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-export const updateSavingsData = (data) => async (dispatch) => {
-  try {
-    dispatch(setSavingsData(data));
-  } catch (err) {
-    console.log(err);
-  }
+  dispatch(setOverviewData({
+    currentSavingSum,
+    incomesSum,
+    mounthSpendingsSum,
+    daySpendingsSum,
+    selectedDaySpendings,
+    moneyRemains,
+  }));
 };
---
+++ src/store/reducer.js
@@ -0,0 +1,83 @@
+import moment from 'moment';
+import 'moment/locale/ru';
+import Type from './action-types';
+
+const initialState = {
+  info: {},
+  date: moment().unix(),
+  isInfoFetched: false,
+  incomes: [],
+  isIncomesFethed: false,
+  costs: [],
+  isCostsFetched: false,
+  mounthSpendings: [],
+  daySpendings: [],
+  isSpendingsFetched: false,
+  savings: [],
+  isSavingsFetched: false,
+  currentSavingSum: null,
+  incomesSum: null,
+  mounthSpendingsSum: null,
+  daySpendingsSum: null,
+  moneyRemains: null,
+};
+
+const reducer = (state = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case Type.SET_USER_INFO:
+      return {
+        ...state,
+        info: payload,
+        isInfoFetched: true,
+      };
+    case Type.SET_MOUNTH_SPENDINGS_DATA:
+      return {
+        ...state,
+        mounthSpendings: payload,
+        isSpendingsFetched: true,
+      };
+    case Type.SET_DAY_SPENDINGS_DATA:
+      return {
+        ...state,
+        daySpendings: payload,
+      };
+    case Type.SET_INCOMES_DATA:
+      return {
+        ...state,
+        incomes: payload,
+        isIncomesFethed: true,
+      };
+    case Type.SET_COSTS_DATA:
+      return {
+        ...state,
+        costs: payload,
+        isCostsFetched: true,
+      };
+    case Type.SET_SAVINGS:
+      return {
+        ...state,
+        savings: payload,
+        isSavingsFetched: true,
+      };
+    case Type.SET_DAILY_BUDJET:
+      return {
+        ...state,
+        dailyBudjet: payload,
+      };
+    case Type.SET_DATE:
+      return {
+        ...state,
+        date: payload,
+      };
+    case Type.SET_OVERVIEW_DATA:
+      return {
+        ...state,
+        ...payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
